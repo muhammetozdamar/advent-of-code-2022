fb293798f879cc3765a3af49e972733eba3652e1
muhammetozdamar <m.ozdmar@gmail.com>
2022-12-13 23:02:17 +0300

    day 6

---
+++ day6/day6part1.py
@@ -0,0 +1,12 @@
+with open("day6\input.txt") as file:
+    signal_raw = file.read().strip()
+    signal_len = len(signal_raw);
+    for i in range(signal_len-3):
+        possible_marker = signal_raw[i:i+4]
+        x=list(set(possible_marker))
+        y=list(possible_marker)
+        x.sort()
+        y.sort()
+        if(x==y):
+            print(i+4)
+            break
